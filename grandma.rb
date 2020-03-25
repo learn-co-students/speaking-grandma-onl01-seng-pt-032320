837bd20da5bddaa83fd2df2924018f39377b3c19
Christian Pardo <chrispardopersonal@gmail.com>
2020-03-25 18:08:13 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  end
-  if phrase != phrase.upcase
+    elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-    else phrase.upcase()
+    elsif phrase.upcase()
     return "NO, NOT SINCE 1938!"
   end
 end
