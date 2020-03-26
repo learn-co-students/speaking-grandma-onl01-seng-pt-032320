db8289c0e1125916e245f03f82b586c0013ae273
Beverly Appleton <bfcain19@gmail.com>
2020-03-26 04:12:11 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,23 @@
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase !=  phrase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.upcase
+    return "NO, NOT SINCE 1938!"
+    
+  end
+  
+
+  
+end
+
+
+
+
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
